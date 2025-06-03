bdd3437613b1bbcbce5e8ffafaaea4d3a1a655c0
Emma Ahlander Klinteberg <eklintis@gmail.com>
2025-06-03 09:07:38 +0200

    secret!

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
-v-container
-  v-row
-    v-col
-      h1 Welcome to your Nuxt + Vuetify + Firebase + Pug Boilerplate! 🎉
+	v-container.d-flex.flex-column.align-center.justify-center(style="min-height: 100vh")
+		h1.text-center Welcome to your Nuxt + Vuetify + Firebase + Pug Boilerplate! 🎉
+		img(src="/boilerplate-welcome.png" alt="Welcome image" style="max-width:1000px;")
 </template>
